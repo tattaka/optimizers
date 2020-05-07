0242f5e4d515c6ce611d76ffd90b324cef7527aa
tattaka <taka_aki@me.com>
2020-05-07 18:51:01 +0900

    fix

--- __init__.py
+++ __init__.py
@@ -35,8 +35,8 @@ def get_optimizer(optimizer: str = 'Adam',
         params = [
             {'params': model.encoder.parameters(), 'lr': lr_e},
         ]
-        for key in model.heads:
-            params.append({'params': getattr(model, key).parameters(), 'lr': lr})
+        # for key in model.heads:
+        params.append({'params': model.heads.parameters(), 'lr': lr})
     else:
         params = [{'params': model.parameters(), 'lr': lr}]
 
